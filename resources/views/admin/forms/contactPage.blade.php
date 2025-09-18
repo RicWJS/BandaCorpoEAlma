11cea0e304f86f5fd8f7685a4014f38f499272f2
RicWJS <162793189+RicWJS@users.noreply.github.com>
2025-09-17 23:25:04 -0300

    sadasd

--- resources/views/admin/forms/contactPage.blade.php
+++
@@ -1,7 +0,0 @@
-{{-- laravel\resources\views\admin\forms\contactPage.blade.php --}}
-@extends('admin.layouts.app')
-
-@section('content')
-    <h1 class="text-2xl font-bold mb-4">Manage Contact Information</h1>
-    <p>Functionality in development.</p>
-@endsection
