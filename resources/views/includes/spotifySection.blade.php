9cb0a2c37cee14bbaf86a602dfb5ea9666e58d94
RicWJS <162793189+RicWJS@users.noreply.github.com>
2025-09-18 02:12:55 -0300

    Update spotifySection.blade.php

--- resources/views/includes/spotifySection.blade.php
+++ resources/views/includes/spotifySection.blade.php
@@ -1,6 +1,6 @@
 {{-- resources/views/includes/spotifySection.blade.php --}}
 
-@if ($spotifyData && $spotifyData->is_visible)
+@if ($spotifyData && ($spotifyData->cover_image_url || $spotifyData->embed_link))
     <section class="spotify-embed-section section-top-border repeat-bg" style="background-image: url('{{ asset('images/logo-pattern.png') }}')">
         <div class="section-wrap">
             <div class="section-container">
@@ -9,15 +9,33 @@
                 </div>
                 
                 <div class="section-content">
-                    <div class="spotify-cover">
-                        <img src="storage/{{ $spotifyData->cover_image_path }}" alt="Capa do Single/Álbum">
-                    </div>
-                    <div class="spotify-embed">
-                        {{-- Iframe dinâmico, só mostra se o link existir --}}
-                        @if ($spotifyData->spotify_link)
-                            <iframe style="border-radius:12px" src="{{ $spotifyData->spotify_link }}" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-                        @else
-                            <p>Em breve, novidades no Spotify!</p>
+                    {{-- Coluna da Imagem (buscando da nova URL da API) --}}
+                    @if ($spotifyData->cover_image_url)
+                        <div class="spotify-cover">
+                            <img src="{{ $spotifyData->cover_image_url }}" alt="Capa do Artista ou Álbum no Spotify">
+                        </div>
+                    @endif
+
+                    {{-- Coluna do Player (usando o novo embed_link) --}}
+                    @if ($spotifyData->embed_link)
+                        <div class="spotify-embed">
+                            @php
+                                // Transforma o link normal do Spotify em um link de embed
+                                $embedUrl = preg_replace(
+                                    '/(https:\/\/open\.spotify\.com)\/(track|artist|album|playlist)\/([a-zA-Z0-9]+)/',
+                                    '$1/embed/$2/$3',
+                                    $spotifyData->embed_link
+                                );
+                            @endphp
+                            <iframe 
+                                style="border-radius:12px" 
+                                src="{{ $embedUrl }}" 
+                                width="100%" 
+                                frameBorder="0" 
+                                allowfullscreen="" 
+                                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                                loading="lazy">
+                            </iframe>
                         @endif
                     </div>
                 </div>
